ab78abfdc9a343e56ea927bc7d418837f06777bd
duongtdn <duongtdn@gmail.com>
2018-11-13 14:47:17 +0000

    add icon and change position of sidebar

--- src/SideBar.js
+++ src/SideBar.js
@@ -14,10 +14,10 @@ class SideBar extends Component {
       const display = this.props.show ? 'block' : 'none';
       return (
         <div className='w3-sidebar w3-bar-block w3-white w3-card-4' 
-             style={{ width: '300px', right: 0, zIndex:1000, display }}
+             style={{ width: '300px', right: 0, top: 0, zIndex: 1000, display }}
         >
-          <span className="w3-button w3-display-topright" onClick={this.props.close}>X</span>
-          <div className="w3-bar-item sidebar-user">
+          <span className="w3-button w3-display-topright w3-red" onClick={this.props.close}>X</span>
+          <div className="w3-bar-item sidebar-user" style={{ padding: '16px' }}>
             <img src={user.profile.picture}
                  className="w3-image w3-round " 
                  width={60} height={60}
@@ -27,8 +27,8 @@ class SideBar extends Component {
 
           <hr />
 
-          <button className="w3-bar-item w3-button" onClick= {this.onClick('dashboard')}> Dash Board </button>
-          <button className="w3-bar-item w3-button" onClick= {this.onClick('app')}> Study Room </button>
+          <button className="w3-bar-item w3-button" onClick= {this.onClick('dashboard')}> <i className="fa fa-area-chart w3-text-blue" /> Dash Board </button>
+          <button className="w3-bar-item w3-button" onClick= {this.onClick('app')}> <i className="fa fa-laptop w3-text-blue" /> Study Room </button>
 
           <hr />
 
@@ -36,7 +36,7 @@ class SideBar extends Component {
             <button className="w3-button w3-border w3-border-orange w3-hover-orange w3-hover-opacity w3-margin" 
                         onClick={this.props.logout}
               > 
-                Logout 
+                <i className="fa fa-sign-out" /> Logout 
             </button>
           </div>
         </div>
